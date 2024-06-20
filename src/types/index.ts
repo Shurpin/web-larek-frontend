0c3fcf7d8ca8cfecaa623e8639a2c4e65931e69b
Ivan Shurpin <ivanshurpin@yandex.ru>
2024-06-20 21:28:54 +0300

    пр8 документация и данные

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,50 +1,42 @@
-// Типы данных
-type ProductCategory = 'другое' | 'софт-скил' | 'дополнительное' | 'кнопка' | 'хфрд-скил';
-
-export interface IProduct {
-  id: string;
-  description: string;
-  image: string;
-  title: string;
-  category: ProductCategory;
-  price: number | null;
+interface IProduct {
+  id: string; // "854cef69-976d-4c2a-a18c-2aa45046c390",
+  description: string; // "Если планируете решать задачи в тренажёре, берите два.",
+  image: string; // "/5_Dots.svg",
+  title: string; // "+1 час в сутках",
+  category: string; // "софт-скил",
+  price: number | null; // 750
 }
 
- type OrderPayment = 'online' | 'cash';
- 
-export interface IOrder {
-  payment: OrderPayment;
-  email: string;
-  phone: string;
-  address: string;
-  total: number; 
-  items: IProduct;
+interface IProducts {
+  total: number; // 10
+  items: IProduct[];
 }
 
-// Отображение прподукта на главной странице 
-type TProduct = Omit<IProduct, "description">;
-// Отображение прподукта в корзине
-type TBasketProduct = Pick<IProduct, "id" | "title" | "price">;
-
-export interface IOrderResult {
-  id: string;
-  total: number; 
+interface IErrorResponse {
+  error: string;
 }
 
-export interface WebLarekApi {
-  getProducts: () => Promise<IProduct[]>;
-  getProductById: (id: string) => Promise<IProduct>;
-  orderProducts: (data: IOrder) => Promise<IOrderResult>;
+enum OrderPayment { // 'online' | 'cash',
+  ONLINE = 'online',
+  CASH = 'cash',
 }
 
-// export interface IToDoList {
-//    addItem(item: IToDoItem): void;
-//    editItem(id:number, data: Partial<IToDoItem>): void; //для редактирования дела
-//    checkItem(id: number): void; //проверили дело выполнено или нет
-//    deleteItem(id: number): void;
-//    setItems(items: IToDoItem[]): void; // загрузить масив дел
-//    getItem(id: number): IToDoItem; //получать по id одну карточку
-//    getItems(): IToDoItem[]; //получать по id все карточки
-//    getTotal(): number; //количество всех 
-//    getDone():number; // количество выполненных 
-// }
+interface IOrderResponse {
+  id: string; // "28c57cb4-3002-4445-8aa1-2a06a5055ae5",
+  total: number; // 2200
+}
+
+interface IOrderPayload {
+  payment: OrderPayment; // 'online' | 'cash',
+  email: string; // "test@test.ru",
+  phone: string; // "+71234567890",
+  address: string; // "Spb Vosstania 1",
+  total: number; // 2200,
+  items: string[];
+}
+
+export interface IMethods {
+  getProducts: () => Promise<IProducts>;
+  getProduct: (id: string) => Promise<IProduct | IErrorResponse>;
+  submitBasket: (payload: IOrderPayload) => Promise<IOrderResponse | IErrorResponse>;
+}
